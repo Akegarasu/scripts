ecd5a07251efae56fe4d91ffbc0e9169f15e22a1
Akiba <akegarasu.akiba@gmail.com>
2022-09-04 21:14:32 +0800

    feat: 支持指定id模式 随机模式

--- anti-zhihu-watermark.user.js
+++ anti-zhihu-watermark.user.js
@@ -13,10 +13,28 @@
 // ==/UserScript==
 
 (function () {
+
+    // runMode自定义运行模式，1为去除，2为随机，3为指定为下方的userId
+    let runMode = 1;
+    let userId = "2b4d16b835f06f373a9dea944eee2dc5"; // 一个随机userId
+
+    const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+    const genWatermark = (uid) => `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 336 75" fill="#fff" fill-opacity="0.005" width="336" height="75" font-size="15px"><text x="10" y="1.5em">${uid}</text><text x="10" y="3em">${Date.now()}</text></svg>`
+    const genStyle = (uid) => `background: url(data:image/svg+xml;base64,${btoa(genWatermark(uid))}) !important`
+
     async function main() {
         await waitForLoaded()
-        document.querySelector("#root > div > div:nth-child(7)").remove()
-        console.log("[盲水印去除]去除完成")
+        if (runMode === 1) {
+            console.log("[盲水印去除] 去除模式")
+            document.querySelector("#root > div > div:nth-child(7)").style.cssText = "background: none !important"
+        } else if (runMode === 2) {
+            console.log("[盲水印去除] 替换为随机userid模式")
+            document.querySelector("#root > div > div:nth-child(7)").style.cssText = genStyle(genRanHex(32))
+        } else if (runMode === 3) {
+            console.log("[盲水印去除] 指定userid模式")
+            document.querySelector("#root > div > div:nth-child(7)").style.cssText = genStyle(userId)
+        }
+        console.log("[盲水印去除] 执行完成")
     }
 
     async function waitForLoaded(timeout = 10 * 1000) {
